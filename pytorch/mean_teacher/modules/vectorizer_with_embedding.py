dafb9fbb1586a94104973600113c53d57f030969
mithun <mithunpaul08@gmail.com>
2019-09-24 20:19:05 -0700

    will exit after printing size of label vocab

--- pytorch/mean_teacher/modules/vectorizer_with_embedding.py
+++ pytorch/mean_teacher/modules/vectorizer_with_embedding.py
@@ -73,8 +73,10 @@ class VectorizerWithEmbedding(object):
         labels_vocab = Vocabulary(add_unk=False)
         for label in sorted(set(claim_ev_df.label)):
             labels_vocab.add_token(label)
-        LOG.debug(f"size of vocabulary is {len(claim_ev_vocab)}")
-      
+        LOG.debug(f"size of claim_ev_vocab is {len(claim_ev_vocab)}")
+        LOG.debug(f"size of labels_vocab is {len(labels_vocab)}")
+        import sys
+        sys.exit(1)
 
         return cls(claim_ev_vocab, labels_vocab)
 

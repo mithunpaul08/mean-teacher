1ae972594717700e4f4a4fb7270d3405ba6e6f51
mithun <mithunpaul08@gmail.com>
2019-10-10 21:11:39 -0700

    previous commit didnt find data files. path was still showing 3 label files. changed now

--- pytorch/mean_teacher/scripts/initializer.py
+++ pytorch/mean_teacher/scripts/initializer.py
@@ -21,8 +21,8 @@ class Initializer():
             fever_dev_local='dev/fever_dev_split_fourlabels.jsonl',
 
             #for server
-            fever_train_server='train/fever_train_delex_smartner_119k_3labels_no_lists_evidence_not_sents.jsonl',
-            fever_dev_server='dev/fever_dev_delexicalized_3labels_26k_no_lists_evidence_not_sents.jsonl',
+            fever_train_server='train/fever_train_split_fourlabels.jsonl',
+            fever_dev_server='dev/fever_dev_split_fourlabels.jsonl',
             data_dir_local='../data/rte/fever',
             data_dir_server='data/rte/fever',
             save_dir='model_storage/ch3/yelp/',

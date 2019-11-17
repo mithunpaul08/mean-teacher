5c9187e95d4a748edc268dcca187adbe09d1a05a
mithun <mithunpaul08@gmail.com>
2019-11-17 08:20:05 -0700

    combined_loss=(1.5*consistency_loss)+(0.5*class_loss_lex)

--- pytorch/mean_teacher/model/train_rao.py
+++ pytorch/mean_teacher/model/train_rao.py
@@ -241,12 +241,11 @@ class Trainer():
                     consistency_loss = consistency_criterion(y_pred_lex, y_pred_delex)
                     consistency_loss_value = consistency_loss.item()
                     running_consistency_loss += (consistency_loss_value - running_consistency_loss) / (batch_index + 1)
-                    combined_class_loss=class_loss_lex+class_loss_delex
 
+                    combined_class_loss=class_loss_lex+class_loss_delex
                     combined_loss=(1.5*consistency_loss)+(0.5*class_loss_lex)
                     combined_loss.backward()
-                    #class_loss_lex.backward()
-
+                    
 
 
                     # step 5. use optimizer to take gradient step

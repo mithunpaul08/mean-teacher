b8f36529debc1c7e8fc2067ca328db1932adcb04
mithun <mithunpaul08@gmail.com>
2020-01-18 21:12:58 -0700

    added test file to shell script getdata.sh

--- student_teacher/get_data.sh
+++ student_teacher/get_data.sh
@@ -2,14 +2,20 @@
 
 #pick according to which one you want to train, dev, test on
 
-
+#train
 mkdir -p data/rte/fever/train/
-mkdir -p data/rte/fever/dev/
-mkdir -p data/rte/fnc/train/
-mkdir -p data/rte/fnc/dev/
-mkdir -p data/rte/fnc/test/
 wget https://storage.googleapis.com/fact_verification_mithun_files/fever_train_lex_4labels.jsonl -O data/rte/fever/train/fever_train_lex.jsonl
 wget https://storage.googleapis.com/fact_verification_mithun_files/fever_train_delex_oaner_4labels.jsonl -O data/rte/fever/train/fever_train_delex.jsonl
+
+#dev
+mkdir -p data/rte/fever/dev/
 wget https://storage.googleapis.com/fact_verification_mithun_files/fever_dev_lex_4labels.jsonl -O data/rte/fever/dev/fever_dev_lex.jsonl
 wget https://storage.googleapis.com/fact_verification_mithun_files/fever_dev_delex_oaner_split_4labels.jsonl -O data/rte/fever/dev/fever_dev_delex.jsonl
-wget https://storage.googleapis.com/fact_verification_mithun_files/fnc_delexicalized/person-c1/fnc_train_delex_4labels.jsonl -O data/rte/fnc/train/fnc_train_delex.jsonl
+
+
+#mkdir -p data/rte/fnc/train/
+#wget https://storage.googleapis.com/fact_verification_mithun_files/fnc_delexicalized/person-c1/fnc_train_delex_4labels.jsonl -O data/rte/fnc/train/fnc_train_delex.jsonl
+#mkdir -p data/rte/fnc/dev/
+#test
+mkdir -p data/rte/fnc/test/
+wget https://storage.googleapis.com/fact_verification_mithun_files/fnc_delexicalized/person-c1/fnc_test_delex.jsonl -O data/rte/fnc/test/fnc_test_delex.jsonl

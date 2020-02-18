fc4a65cdd2bd6a184073042109702d71a732c34b
mithun <mithunpaul08@gmail.com>
2020-02-17 22:45:32 -0700

    random_seed=3737,

--- student_teacher/mean_teacher/scripts/initializer.py
+++ student_teacher/mean_teacher/scripts/initializer.py
@@ -51,7 +51,7 @@ class Initializer():
             early_stopping_criteria=5,
             learning_rate=0.005,
             num_epochs=500,
-            random_seed=256,
+            random_seed=3737,
             weight_decay=5e-5,
             Adagrad_init=0,
 

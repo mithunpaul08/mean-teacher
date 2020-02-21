abdbe1725c60c80950ebda0ca058f1b673cedfbc
mithun <mithunpaul08@gmail.com>
2020-02-20 17:50:09 -0700

    changed shell script to get the right fnc dev file

--- student_teacher/get_data_run.sh
+++ student_teacher/get_data_run.sh
@@ -7,14 +7,14 @@ mkdir -p data/rte/fever/train/
 
 FILE=data/rte/fever/train/fever_train_lex.jsonl
 if test -f "$FILE";then
-    echo "$FILE exist"
+    echo "$FILE exists"
 else
     wget https://storage.googleapis.com/fact_verification_mithun_files/fever_train_lex_4labels.jsonl -O $FILE
 fi
 
 FILE=data/rte/fever/train/fever_train_delex.jsonl
 if test -f "$FILE";then
-    echo "$FILE exist"
+    echo "$FILE exists"
 else
     wget https://storage.googleapis.com/fact_verification_mithun_files/fever_train_delex_oaner_4labels.jsonl -O $FILE
 fi
@@ -24,34 +24,35 @@ fi
 mkdir -p data/rte/fever/dev/
 FILE=data/rte/fever/dev/fever_dev_lex.jsonl
 if test -f "$FILE";then
-    echo "$FILE exist"
+    echo "$FILE exists"
 else
     wget https://storage.googleapis.com/fact_verification_mithun_files/fever_dev_lex_4labels.jsonl -O $FILE
 fi
 
 FILE=data/rte/fever/dev/fever_dev_delex.jsonl
 if test -f "$FILE";then
-    echo "$FILE exist"
+    echo "$FILE exists"
 else
     wget https://storage.googleapis.com/fact_verification_mithun_files/fever_dev_delex_oaner_split_4labels.jsonl -O $FILE
 fi
 
 
 #######test
-mkdir -p data/rte/fnc/test/
-FILE=data/rte/fnc/test/fnc_test_delex.jsonl
+mkdir -p data/rte/fnc/dev/
+FILE=data/rte/fnc/dev/fnc_dev_delex.jsonl
 if test -f "$FILE";then
-    echo "$FILE exist"
+    echo "$FILE exists"
 else
-    wget https://storage.googleapis.com/fact_verification_mithun_files/fnc_delexicalized/person-c1/fnc_test_delex.jsonl -O $FILE
+    wget https://storage.googleapis.com/fact_verification_mithun_files/fnc_delexicalized/person-c1/fnc_delexicalized_person-c1_dev_mithun_carved_from_train.jsonl -O $FILE
 fi
 
 mkdir -p log_dir/
 
-#below are the paths for fnc dev and fnc train
+#below are the paths for fnc test and  train partitions
 #mkdir -p data/rte/fnc/train/
 #wget https://storage.googleapis.com/fact_verification_mithun_files/fnc_delexicalized/person-c1/fnc_train_delex_4labels.jsonl -O data/rte/fnc/train/fnc_train_delex.jsonl
-#mkdir -p data/rte/fnc/dev/
+#mkdir -p data/rte/fnc/test/
+#wget https://storage.googleapis.com/fact_verification_mithun_files/fnc_delexicalized/person-c1/fnc_delexicalized_person-c1_actual_fnc_test.jsonl -O data/rte/fnc/test/fnc_test_delex.jsonl
 
 
 

f7f001d24af2d5ff419e196939d64b95124656aa
mithun <mithunpaul08@gmail.com>
2020-02-22 14:58:55 -0700

    changed the shell script ot use gpu2

--- student_teacher/get_data_run.sh
+++ student_teacher/get_data_run.sh
@@ -73,6 +73,6 @@ mkdir -p log_dir/
 
 
 
-python main.py --add_student True --which_gpu_to_use 0  --use_ema False \
---load_model_from_disk_and_test True \
+python main.py --add_student True --which_gpu_to_use 2  --use_ema False \
+--load_model_from_disk_and_test False \
 --lex_train_full_path fever/train/fever_train_lex.jsonl

430644dd2a3b1ad03b5aa797133be6500b8f36bb
mithun <mithunpaul08@gmail.com>
2020-02-27 01:26:31 -0700

    added ocelote boiler plate code

--- student_teacher/README.md
+++ student_teacher/README.md
@@ -11,7 +11,8 @@ In this fork of the original mean teacher code, we replace the feed forward netw
 ```
 conda create --name mean_teacher python=3 numpy scipy pandas nltk tqdm
 source activate mean_teacher
-pip sklearn
+pip install sklearn
+pip install comet_ml
 pip install git+ssh://git@github.com/pytorch/vision@c31c3d7e0e68e871d2128c8b731698ed3b11b119
 pip install gitpython
 conda install pytorch-cpu torchvision-cpu -c pytorch *
--- student_teacher/get_data_run.sh
+++ student_teacher/get_data_run.sh
@@ -1,4 +1,28 @@
-#!/usr/bin/env bash
+Add this on top of your existing shell script:
+
+#!/bin/bash
+# Your job will use 1 node, 28 cores, and 168gb of memory total.
+#PBS -q windfall
+#PBS -l select=1:ncpus=28:mem=168gb:pcmem=8gb:ngpus=1
+### Specify a name for the job
+#PBS -N 1_contextual
+### Specify the group name
+#PBS -W group_list=msurdeanu
+### Used if job requires partial node only
+#PBS -l place=pack:exclhost
+### CPUtime required in hhh:mm:ss.
+### Leading 0's can be omitted e.g 48:0:0 sets 48 hours
+#PBS -l cput=1344:00:00
+### Walltime is how long your job will run
+#PBS -l walltime=48:00:00
+#PBS -e /extra/mithunpaul/error/
+#PBS -o /extra/mithunpaul/output/
+
+#####module load cuda80/neuralnet/6/6.0
+#####module load cuda80/toolkit/8.0.61
+module load singularity/2/2.6.1
+
+cd $PBS_O_WORKDIR
 
 #pick according to which kind of dataset you want to use for  train, dev, test on. Eg: train on fever, test on fnc
 

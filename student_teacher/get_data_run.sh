4ccb06f4dad4f08570125dc6837722c6a3ff792b
mithun <mithunpaul08@gmail.com>
2020-02-27 23:37:38 -0700

    got error in the previous commit that it cant find parent repo. this is probably because i was giving the hard coded path for main.py. now will try a python ./main.py instead

--- student_teacher/get_data_run.sh
+++ student_teacher/get_data_run.sh
@@ -114,6 +114,6 @@ mkdir -p log_dir/
 
 
 
-python /extra/mithunpaul/mean-teacher/student_teacher/main.py --add_student True --which_gpu_to_use 0  --use_ema False \
+python ./main.py --add_student True --which_gpu_to_use 0  --use_ema False \
 --load_model_from_disk_and_test False \
 --lex_train_full_path fever/train/fever_train_lex.jsonl

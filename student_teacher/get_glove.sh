4b75bf48cc7facac138bcea286797a912a05eb2e
mithun <mithunpaul08@gmail.com>
2020-03-01 22:14:30 -0700

    glove file was downloading and expanding glove every single time. wont do that now if the glove file already exists

--- student_teacher/get_glove.sh
+++ student_teacher/get_glove.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 mkdir -p data
 mkdir -p data/glove
-wget http://nlp.stanford.edu/data/glove.840B.300d.zip
-unzip glove.840B.300d.zip -d data/glove
+
+FILE=data/glove/glove.840B.300d.txt
+if test -f "$FILE";then
+    echo "$FILE exists"
+else
+    wget http://nlp.stanford.edu/data/glove.840B.300d.zip
+    unzip glove.840B.300d.zip -d data/glove
+fi
+
+

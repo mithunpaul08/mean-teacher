9944aa882ff55781771b4649625da04460d91eba
mithun <mithunpaul08@gmail.com>
2020-03-13 01:05:13 -0700

    added nn code

--- nn/eval_noalnlp/get_data_delex.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p data/rte/fnc/test/
-mkdir -p model_storage/
-wget https://storage.googleapis.com/fact_verification_mithun_files/fnc_test_delex_oaner_4labels.jsonl -O data/rte/fnc/test/fn_test_split_fourlabels.jsonl
--- nn/eval_noalnlp/mean_teacher/utils/logger.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-logging.basicConfig(filename='mean_teacher.log', filemode='w+')
-LOG = logging.getLogger('main')
-
-

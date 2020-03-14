0c5cca21dd71a9194a245d0c487b8e36dd7907bd
mithun <mithunpaul08@gmail.com>
2020-03-14 14:55:50 -0700

    seed=23233,this run hadn't completed last time

--- student_teacher/run_on_hpc.sh
+++ student_teacher/run_on_hpc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Your job will use 1 node, 28 cores, and 168gb of memory total.
 #PBS -q standard
-#PBS -l select=1:ncpus=16:mem=224gb:np100s=1:os7=True
+#PBS -l select=1:ncpus=28:mem=224gb:np100s=1:os7=True
 ### Specify a name for the job
 #PBS -N mithuns_meanteacher
 ### Optional. Request email when job begins and ends - commented out in this case

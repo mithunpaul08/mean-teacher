8b02d70dd32283418d51b35b7c072f7ae80c3783
mithun <mithunpaul08@gmail.com>
2020-03-15 23:52:46 -0700

    minor change to the intial starter shell script. had missed out reduce size of one file

--- student_teacher/reduce_size.sh
+++ student_teacher/reduce_size.sh
@@ -21,3 +21,7 @@ mv temp data/rte/fnc/test/fnc_test_lex.jsonl
 head -20 data/rte/fnc/dev/fnc_dev_lex.jsonl > temp
 mv temp data/rte/fnc/dev/fnc_dev_lex.jsonl
 
+
+head -20 data/rte/fnc/dev/fnc_dev_delex.jsonl > temp
+mv temp data/rte/fnc/dev/fnc_dev_delex.jsonl
+

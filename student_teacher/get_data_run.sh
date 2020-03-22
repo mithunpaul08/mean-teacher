980adfb1afea45fb3e7275bf768af4deb1941725
mithun <mithunpaul08@gmail.com>
2020-03-21 22:17:27 -0700

    removing which gpu to use

--- student_teacher/get_data_run.sh
+++ student_teacher/get_data_run.sh
@@ -85,6 +85,6 @@ mkdir -p log_dir/
 
 
 
-python main.py --add_student True --which_gpu_to_use 0  --use_ema False \
+python main.py --add_student True  --use_ema False \
 --load_model_from_disk_and_test False \
 --lex_train_full_path fever/train/fever_train_lex.jsonl

d251dd1548fd4474303164ae6256026dfef7497a
mithun <mithunpaul08@gmail.com>
2020-03-29 22:07:05 -0700

    moving the loading teacher model and vectorizer code to the very first thing in main. this to avoid it from calling the Dataset from disk

--- student_teacher/main.py
+++ student_teacher/main.py
@@ -73,19 +73,35 @@ glove_filepath_in, lex_train_input_file, lex_dev_input_file, lex_test_input_file
 
 
 LOG.info(f"{current_time:}Going to read data")
+classifier_teacher_lex=None
 
+# trial on march 2020 to 1) train a teacher model offline then 2) load that trained model as teacher (which doesn't have a backprop)
+# and then try training student.
 if(args.use_trained_teacher_inside_student_teacher_arch):
-    #loading data again, but this is just to instantiate/load the saved vectorizer. Could have separated it out and avoided loading input twice, but too deeply embedded code
-    # dataset_loaded = RTEDataset.load_dataset_and_load_vectorizer(lex_train_input_file, lex_dev_input_file, delex_train_input_file, delex_dev_input_file, delex_test_input_file,
-    #      lex_test_input_file, args,args.vectorizer_file)
-    #
-    # vectorizer_loaded = dataset_loaded.get_vectorizer()
-
     # when you are using a trained model, you should really be using the same vectorizer. Else embedding mismatch will happen
     vectorizer_loaded= RTEDataset.load_vectorizer_only(args.vectorizer_file)
 
     LOG.info(f"num_classes_in={len(vectorizer_loaded.label_vocab)}")
     LOG.info(f"word_vocab_size={len(vectorizer_loaded.claim_ev_vocab)}")
+
+
+
+    words = vectorizer_loaded.claim_ev_vocab._token_to_idx.keys()
+    embeddings_loaded, embedding_size_loaded = make_embedding_matrix(glove_filepath_in, words)
+
+    LOG.info(f"wordemb_size_in={(embedding_size_loaded)}")
+    LOG.info(f"len word_vocab_embed={len(embeddings_loaded)}")
+    classifier_teacher_lex = create_model(logger_object=LOG, args_in=args,
+                                          num_classes_in=len(vectorizer_loaded.label_vocab)
+                                          , word_vocab_embed=embeddings_loaded,
+                                          word_vocab_size=len(vectorizer_loaded.claim_ev_vocab),
+                                          wordemb_size_in=embedding_size_loaded)
+
+    assert os.path.exists(args.trained_model_path) is True
+    assert os.path.isfile(args.trained_model_path) is True
+    if os.path.getsize(args.trained_model_path) > 0:
+        classifier_teacher_lex.load_state_dict(
+            torch.load(args.trained_model_path, map_location=torch.device(args.device)))
 
 if args.reload_data_from_files:
     # training from a checkpoint
@@ -111,46 +127,18 @@ else:
     embeddings = None
 
 num_features=len(vectorizer.claim_ev_vocab)
-classifier_teacher_lex=None
-
-#trial on march 2020 to 1) train a teacher model offline then 2) load that trained model as teacher (which doesn't have a backprop)
-#and then try training student.
-if(args.use_trained_teacher_inside_student_teacher_arch):
-    #loading data again, but this is just to instantiate/load the saved vectorizer. Could have separated it out and avoided loading input twice, but too deeply embedded code
-    # dataset_loaded = RTEDataset.load_dataset_and_load_vectorizer(lex_train_input_file, lex_dev_input_file, delex_train_input_file, delex_dev_input_file, delex_test_input_file,
-    #      lex_test_input_file, args,args.vectorizer_file)
-    #
-    # vectorizer_loaded = dataset_loaded.get_vectorizer()
-
-    # when you are using a trained model, you should really be using the same vectorizer. Else embedding mismatch will happen
-    vectorizer_loaded= RTEDataset.load_vectorizer_only(args.vectorizer_file)
-
-    LOG.info(f"num_classes_in={len(vectorizer_loaded.label_vocab)}")
-    LOG.info(f"word_vocab_size={len(vectorizer_loaded.claim_ev_vocab)}")
-    LOG.info(f"wordemb_size_in={(embedding_size)}")
-    LOG.info(f"len word_vocab_embed={len(embeddings)}")
-
-    classifier_teacher_lex = create_model(logger_object=LOG, args_in=args, num_classes_in=len(vectorizer_loaded.label_vocab)
-                                          , word_vocab_embed=embeddings, word_vocab_size=len(vectorizer_loaded.claim_ev_vocab),
-                                          wordemb_size_in=embedding_size)
-
-    assert os.path.exists(args.trained_model_path) is True
-    assert os.path.isfile(args.trained_model_path) is True
-    if os.path.getsize(args.trained_model_path) > 0:
-        classifier_teacher_lex.load_state_dict(torch.load(args.trained_model_path, map_location=torch.device(args.device)))
 
 
-
-else:
-    #when the teacher is used in ema mode, no backpropagation will occur in teacher.
-    if(args.use_ema):
-        classifier_teacher_lex = create_model(logger_object=LOG, args_in=args, num_classes_in=len(vectorizer.label_vocab)
-                                          , word_vocab_embed=embeddings, word_vocab_size=num_features, wordemb_size_in=embedding_size,ema=True)
-    else:
-
-        classifier_teacher_lex = create_model(logger_object=LOG, args_in=args, num_classes_in=len(vectorizer.label_vocab)
-                                              , word_vocab_embed=embeddings, word_vocab_size=num_features,
-                                              wordemb_size_in=embedding_size)
+# else:
+#     #when the teacher is used in ema mode, no backpropagation will occur in teacher.
+#     if(args.use_ema):
+#         classifier_teacher_lex = create_model(logger_object=LOG, args_in=args, num_classes_in=len(vectorizer.label_vocab)
+#                                           , word_vocab_embed=embeddings, word_vocab_size=num_features, wordemb_size_in=embedding_size,ema=True)
+#     else:
+#
+#         classifier_teacher_lex = create_model(logger_object=LOG, args_in=args, num_classes_in=len(vectorizer.label_vocab)
+#                                               , word_vocab_embed=embeddings, word_vocab_size=num_features,
+#                                               wordemb_size_in=embedding_size)
 
 assert classifier_teacher_lex is not None
 classifier_student_delex = create_model(logger_object=LOG, args_in=args, num_classes_in=len(vectorizer.label_vocab)

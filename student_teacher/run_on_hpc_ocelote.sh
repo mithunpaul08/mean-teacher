1d76360c516698a09bbcbc581b23747c027011ed
mithun <mithunpaul08@gmail.com>
2020-04-24 23:55:30 -0700

    reducing memory to 8gh

--- student_teacher/run_on_hpc_ocelote.sh
+++ student_teacher/run_on_hpc_ocelote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Your job will use 1 node, 28 cores, and 168gb of memory total.
 #PBS -q windfall
-#PBS -l select=1:ncpus=28:mem=224gb:np100s=1:os7=True
+#PBS -l select=1:ncpus=28:mem=8gb:np100s=1:os7=True:pcmem=4gb
 ### Specify a name for the job
 #PBS -N mithuns_meanteacher
 ### Optional. Request email when job begins and ends - commented out in this case

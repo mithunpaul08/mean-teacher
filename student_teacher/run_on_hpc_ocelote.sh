4c7f4be5b4dd31e4661ac449798c1478c3d6df8e
mithun <mithunpaul08@gmail.com>
2020-04-25 03:01:43 -0700

    mem

--- student_teacher/run_on_hpc_ocelote.sh
+++ student_teacher/run_on_hpc_ocelote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Your job will use 1 node, 28 cores, and 168gb of memory total.
 #PBS -q windfall
-#PBS -l select=1:ncpus=28:mem=4gb:os7=True:pcmem=6gb:ngpus=1
+#PBS -l select=1:ncpus=28:mem=168gb:np100s=1:os7=True
 ### Specify a name for the job
 #PBS -N mithuns_meanteacher
 ### Optional. Request email when job begins and ends - commented out in this case

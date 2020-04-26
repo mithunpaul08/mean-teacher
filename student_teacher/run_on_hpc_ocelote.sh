8141c9baa67c49ada43dfda618fe953dd8a6b954
mithun <mithunpaul08@gmail.com>
2020-04-25 21:13:29 -0700

    using script created by script builder. also no gpu

--- student_teacher/run_on_hpc_ocelote.sh
+++ student_teacher/run_on_hpc_ocelote.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-# Your job will use 1 node, 28 cores, and 168gb of memory total.
-#PBS -q windfall
-#PBS -l select=1:ncpus=28:mem=24gb:os7=True
+### Your job will use 1 node, 28 cores, and 168gb of memory total.
+#PBS -q standard
+#PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb
 ### Specify a name for the job
-#PBS -N mithuns_meanteacher
-### Optional. Request email when job begins and ends - commented out in this case
-### PBS -m bea
-### Optional. Specify email address to use for notification - commented out in this case
-### PBS -M mithunpaul@email.arizona.edu
+#PBS -N d
 ### Specify the group name
 #PBS -W group_list=msurdeanu
 ### Used if job requires partial node only
-#PBS -l place=free:shared
-# ## CPUtime required in hhh:mm:ss.
+#PBS -l place=pack:shared
+### CPUtime required in hhh:mm:ss.
 ### Leading 0's can be omitted e.g 48:0:0 sets 48 hours
-#PBS -l cput=8:00:00
+#PBS -l cput=280:00:00
 ### Walltime is how long your job will run
-#PBS -l walltime=8:00:00
+#PBS -l walltime=10:0:00
+### Joins standard error and standard out
+#PBS -j oe
 #PBS -e /extra/mithunpaul/error/
 #PBS -o /extra/mithunpaul/output/
 

ab708f66114b842c85e415b38eceb8f0c904d225
mithun <mithunpaul08@gmail.com>
2020-04-26 12:08:45 -0700

    ngpus=1

--- student_teacher/run_on_hpc_elgato.sh
+++ student_teacher/run_on_hpc_elgato.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Your job will use 1 node, 28 cores, and 168gb of memory total.
 #PBS -q standard
-#PBS -l select=1:ncpus=16:mem=224gb
+#PBS -l select=1:ncpus=16:mem=224gb:ngpus=1
 ### Specify a name for the job
 #PBS -N mithuns_meanteacher
 ### Optional. Request email when job begins and ends - commented out in this case
@@ -14,9 +14,9 @@
 #PBS -l place=pack:exclhost
 ### CPUtime required in hhh:mm:ss.
 ### Leading 0's can be omitted e.g 48:0:0 sets 48 hours
-#PBS -l cput=8:00:00
+#PBS -l cput=48:00:00
 ### Walltime is how long your job will run
-#PBS -l walltime=8:00:00
+#PBS -l walltime=48:00:00
 #PBS -e /extra/mithunpaul/error/
 #PBS -o /extra/mithunpaul/output/
 

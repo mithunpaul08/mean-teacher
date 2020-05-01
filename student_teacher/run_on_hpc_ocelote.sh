40ccd9a17bf103d92c9032528ebde7d100574790
mithun <mithunpaul08@gmail.com>
2020-04-30 21:53:08 -0700

    heard back from hpc consult. looks ilke the issue was my cpu time i h ad requested was small..apparently cpu time is 28 times wall time-which is thato f gpu

--- student_teacher/run_on_hpc_ocelote.sh
+++ student_teacher/run_on_hpc_ocelote.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 # Your job will use 1 node, 28 cores, and 168gb of memory total.
 #PBS -q standard
-#PBS -l select=1:ncpus=28:mem=224gb:np100s=1:os7=True
+#PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1
 ### Specify a name for the job
-#PBS -N mithuns_meanteacher
-### Optional. Request email when job begins and ends - commented out in this case
-# PBS -m bea
-### Optional. Specify email address to use for notification - commented out in this case
-# PBS -M mithunpaul@email.arizona.edu
+#PBS -N mithun_unk_gw50
 ### Specify the group name
 #PBS -W group_list=dkp
 ### Used if job requires partial node only
 #PBS -l place=pack:shared
 ### CPUtime required in hhh:mm:ss.
 ### Leading 0's can be omitted e.g 48:0:0 sets 48 hours
-#PBS -l cput=48:00:00
+#PBS -l cput=247:20:00
 ### Walltime is how long your job will run
-#PBS -l walltime=48:00:00
+#PBS -l walltime=8:50:00
+### Joins standard error and standard out
+#PBS -j oe
 
 #####module load cuda80/neuralnet/6/6.0
 #####module load cuda80/toolkit/8.0.61

022c4b976fca61a0faf10428dc4ec3161f9b4579
mithun <mithunpaul08@gmail.com>
2020-05-01 15:19:31 -0700

    asking for 3 nodes exclusive..

--- student_teacher/run_on_hpc_ocelote.sh
+++ student_teacher/run_on_hpc_ocelote.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 # Your job will use 1 node, 28 cores, and 168gb of memory total.
 #PBS -q standard
-#PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
+#PBS -l select=3:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
+
+
 ### Specify a name for the job
-#PBS -N mithun_unk_gw10
+#PBS -N gw_minfreq_20
 ### Specify the group name
 #PBS -W group_list=msurdeanu
 ### Used if job requires partial node only
-#PBS -l place=pack:exclhost
+#PBS -l place=free:exclhost
 ### CPUtime required in hhh:mm:ss.
 ### Leading 0's can be omitted e.g 48:0:0 sets 48 hours
-#PBS -l cput=247:20:00
+#PBS -l cput=672:00:00
 ### Walltime is how long your job will run
-#PBS -l walltime=8:50:00
+#PBS -l walltime=8:00:00
 ### Joins standard error and standard out
 #PBS -j oe
-
 
 ### Optional. Request email when job begins and ends
 # PBS -m bea

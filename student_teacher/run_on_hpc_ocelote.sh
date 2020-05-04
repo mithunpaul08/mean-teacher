d746562a1e2024c88601643a3a83f929d00bb52e
mithun <mithunpaul08@gmail.com>
2020-05-04 14:13:40 -0700

    there was an error in ocelote run script. fixed it

--- student_teacher/run_on_hpc_ocelote.sh
+++ student_teacher/run_on_hpc_ocelote.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 # Your job will use 1 node, 28 cores, and 168gb of memory total.
 #PBS -q standard
-#PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
+#PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1
 ### Specify a name for the job
-#PBS -N job_name
-### Specify the group name
-#PBS -W group_list=1s_1t_random_seed=32
+#PBS -N 1s_1t_random_seed=32
 ### Specify the group name
 #PBS -W group_list=msurdeanu
 ### Used if job requires partial node only
 #PBS -l place=pack:shared
 ### CPUtime required in hhh:mm:ss.
 ### Leading 0's can be omitted e.g 48:0:0 sets 48 hours
-#PBS -l cput=224:00:00
+#PBS -l cput=672:00:00
 ### Walltime is how long your job will run
-#PBS -l walltime=8:00:00
+#PBS -l walltime=24:00:00
 ### Joins standard error and standard out
 #PBS -j oe
 

a1016fcb916fa063ab0025ff4c53763f3bbc6982
mithun <mithunpaul08@gmail.com>
2020-05-05 02:10:24 -0700

    random_seed=32 again

--- student_teacher/mean_teacher/scripts/initializer.py
+++ student_teacher/mean_teacher/scripts/initializer.py
@@ -57,7 +57,7 @@ class Initializer():
             early_stopping_criteria=5,
             learning_rate=0.005,
             num_epochs=10000,
-            random_seed=9567349,
+            random_seed=32,
 
             weight_decay=5e-5,
             Adagrad_init=0,
--- student_teacher/run_on_hpc_ocelote.sh
+++ student_teacher/run_on_hpc_ocelote.sh
@@ -3,7 +3,7 @@
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
 ### Specify a name for the job
-#PBS -N 9567349_random_seed_1s_3t
+#PBS -N 32_random_seed_1s_3t
 ### Specify the group name
 #PBS -W group_list=msurdeanu
 ### Used if job requires partial node only

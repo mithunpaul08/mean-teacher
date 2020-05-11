3624c1118db2c9276fefcebeb1657568da7011fc
mithun <mithunpaul08@gmail.com>
2020-05-10 21:34:47 -0700

    reducing number of cpus

--- student_teacher/run_on_hpc_elgato.sh
+++ student_teacher/run_on_hpc_elgato.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Your job will use 1 node, 28 cores, and 168gb of memory total.
 #PBS -q standard
-#PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb
+#PBS -l select=1:ncpus=16:mem=62gb:pcmem=4gb
 ### Specify a name for the job
 #PBS -N elgato_
 ### Specify the group name

0ebe4a8d74b9ab73343e911c66e048b899f5c743
mithun <mithunpaul08@gmail.com>
2020-05-10 17:50:58 -0700

    no gpu

--- student_teacher/run_on_hpc_ocelote.sh
+++ student_teacher/run_on_hpc_ocelote.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-# Your job will use 1 node, 28 cores, and 168gb of memory total.
-#PBS -q windfall
-#PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
+### Your job will use 1 node, 28 cores, and 168gb of memory total.
+#PBS -q standard
+#PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb
 ### Specify a name for the job
-#PBS -N 32_random_seed_1s_3t
+#PBS -N 3t_tuning_0.5_0.5_8
 ### Specify the group name
 #PBS -W group_list=msurdeanu
 ### Used if job requires partial node only
 #PBS -l place=pack:shared
 ### CPUtime required in hhh:mm:ss.
 ### Leading 0's can be omitted e.g 48:0:0 sets 48 hours
-#PBS -l cput=336:00:00
+#PBS -l cput=672:00:00
 ### Walltime is how long your job will run
 #PBS -l walltime=24:00:00
 ### Joins standard error and standard out
-#PBS -j oe
+#PBS -j o
 
 ### Optional. Request email when job begins and ends
 # PBS -m bea

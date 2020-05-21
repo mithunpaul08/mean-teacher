5d1dc27dc92720f5ead2cfb63fb6d9e91baddbed
mithun <mithunpaul08@gmail.com>
2020-05-20 23:08:25 -0700

    removing glove and also new comet graph=False

--- student_teacher/main.py
+++ student_teacher/main.py
@@ -53,9 +53,6 @@ args=initializer.set_default_parameters2(args)
 
 
 
-if(args.load_model_from_disk_and_test):
-    args.lex_test='fnc/test/fnc_test_lex.jsonl'
-    args.delex_test='fnc/test/fnc_test_delex.jsonl'
 
 current_time={time.strftime("%c")}
 logger_client=Logger()
@@ -73,22 +70,12 @@ if (comet_value_updater) is not None:
 
 
 
-
-
-
-if args.run_type=="test":
-    args.load_vectorizer=True
-    args.load_model_from_disk=True
-
-
-
 LOG.info(f"setting the manual seed as {args.random_seed} ")
 LOG.setLevel(args.log_level)
 
-current_time={time.strftime("%c")}
 
-glove_filepath_in, lex_train_input_file, lex_dev_input_file, lex_test_input_file , delex_train_input_file, \
-delex_dev_input_file, delex_test_input_file,gigaword_full_path=initializer.get_file_paths(LOG)
+# glove_filepath_in, lex_train_input_file, lex_dev_input_file, lex_test_input_file , delex_train_input_file, \
+# delex_dev_input_file, delex_test_input_file,gigaword_full_path=initializer.get_file_paths(LOG)
 
 
 LOG.info(f"{current_time:}Going to read data")
@@ -108,41 +95,41 @@ LOG.info(f"cuda available:{avail}")
 # - merge them- set{trainwords,gigawords}
 # - for each word load its glove embedding
 #
-vectorizer_with_embedding.gigaword_freq=read_gigaword_freq_file(gigaword_full_path,args.gw_minfreq)
+#vectorizer_with_embedding.gigaword_freq=read_gigaword_freq_file(gigaword_full_path,args.gw_minfreq)
 
-
-if args.reload_data_from_files:
-    # training from a checkpoint
-    dataset = RTEDataset.load_dataset_and_load_vectorizer(args.fever_lex_train,
-                                                              args.vectorizer_file)
-else:
-    # create dataset and vectorizer
-    dataset = RTEDataset.load_dataset_and_create_vocabulary_for_combined_lex_delex(lex_train_input_file, lex_dev_input_file, delex_train_input_file, delex_dev_input_file, delex_test_input_file, lex_test_input_file,args)
-    vectorizer_name=os.path.join(args.save_dir,"vectorizer_"+sha+".json")
-    dataset.save_vectorizer(vectorizer_name)
-vectorizer = dataset.get_vectorizer()
+#
+# if args.reload_data_from_files:
+#     # training from a checkpoint
+#     dataset = RTEDataset.load_dataset_and_load_vectorizer(args.fever_lex_train,
+#                                                               args.vectorizer_file)
+# else:
+#     # create dataset and vectorizer
+#     dataset = RTEDataset.load_dataset_and_create_vocabulary_for_combined_lex_delex(lex_train_input_file, lex_dev_input_file, delex_train_input_file, delex_dev_input_file, delex_test_input_file, lex_test_input_file,args)
+#     vectorizer_name=os.path.join(args.save_dir,"vectorizer_"+sha+".json")
+#     dataset.save_vectorizer(vectorizer_name)
+# vectorizer = dataset.get_vectorizer()
 
 # taking embedding size from user initially, but will get replaced by original embedding size if its loaded
-embedding_size=args.embedding_size
-
-# Use GloVe or randomly initialized embeddings
-LOG.info(f"{current_time} going to load glove from path:{glove_filepath_in}")
-if args.use_glove:
-    words = vectorizer.claim_ev_vocab._token_to_idx.keys()
-    embeddings,embedding_size = make_embedding_matrix(glove_filepath_in,words)
-
-    # Create a vocabulary dictionary for the cross domain dataset and load its embeddings also in memory.
-    # Reason: it was noticed that when we were testing on the cross domain dataset, there were way too many
-    # unknown <UNK> tokens. So now, if a word is new in the cross domain, don't call it UNK just because it was not
-    # there in in-domain vocabulary.  check if it exists in crossdomain vocab. if yes, load its glove
-    # make_embedding_matrix()
-
-    LOG.info(f"{current_time:} Using pre-trained embeddings")
-else:
-    LOG.info(f"{current_time:} Not using pre-trained embeddings")
-    embeddings = None
-
-num_features=len(vectorizer.claim_ev_vocab)
+# embedding_size=args.embedding_size
+#
+# # Use GloVe or randomly initialized embeddings
+# LOG.info(f"{current_time} going to load glove from path:{glove_filepath_in}")
+# if args.use_glove:
+#     words = vectorizer.claim_ev_vocab._token_to_idx.keys()
+#     embeddings,embedding_size = make_embedding_matrix(glove_filepath_in,words)
+#
+#     # Create a vocabulary dictionary for the cross domain dataset and load its embeddings also in memory.
+#     # Reason: it was noticed that when we were testing on the cross domain dataset, there were way too many
+#     # unknown <UNK> tokens. So now, if a word is new in the cross domain, don't call it UNK just because it was not
+#     # there in in-domain vocabulary.  check if it exists in crossdomain vocab. if yes, load its glove
+#     # make_embedding_matrix()
+#
+#     LOG.info(f"{current_time:} Using pre-trained embeddings")
+# else:
+#     LOG.info(f"{current_time:} Not using pre-trained embeddings")
+#     embeddings = None
+#
+# num_features=len(vectorizer.claim_ev_vocab)
 
 
 #You can specify any huggingface/transformers pre-trained model here, for example, bert-base-uncased, roberta-base, xlm-roberta-base
@@ -157,13 +144,8 @@ train_num_labels = nli_reader_fever.get_num_labels()
 model_save_path = 'output/training_nli_'+model_name.replace("/", "-")+'-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 
-classifier_teacher_lex=None
-if(args.use_ema):
-    classifier_teacher_lex = create_model_bert(logger_object=LOG, args_in=args, num_classes_in=len(vectorizer.label_vocab)
-                                      , word_vocab_embed=embeddings, word_vocab_size=num_features, wordemb_size_in=embedding_size,ema=True)
-else:
 
-    classifier_teacher_lex = create_model_bert()
+classifier_teacher_lex = create_model_bert()
 
 
 # classifier_student_delex_ema = create_model(logger_object=LOG, args_in=args, num_classes_in=len(vectorizer.label_vocab)

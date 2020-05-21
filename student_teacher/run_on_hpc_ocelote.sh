f683a1978232d77381be864d957896a549618e36
mithun <mithunpaul08@gmail.com>
2020-05-20 23:30:01 -0700

    python export path was old one

--- student_teacher/run_on_hpc_ocelote.sh
+++ student_teacher/run_on_hpc_ocelote.sh
@@ -11,21 +11,24 @@ module load cuda90/neuralnet/7/7.3.1.20
 module load python/3.6/3.6.5
 
 #uncomment this if you don't want to reinstall venv- usually you just have to do this only once ever
-rm -rf my_virtual_env
-mkdir my_virtual_env
-python3 -m venv my_virtual_env
-source my_virtual_env/bin/activate
-pip install --upgrade pip
-pip install torch==1.5.0+cu92 torchvision==0.6.0+cu92 -f https://download.pytorch.org/whl/torch_stable.html
-cd /home/u11/mithunpaul/mean-teacher/student_teacher
-pip install -r requirements.txt
-
+#rm -rf my_virtual_env
+#mkdir my_virtual_env
+#python3 -m venv my_virtual_env
 
 #this is the only line you need if you already have a virtual_env set up
+source my_virtual_env/bin/activate
+
+
+#pip install --upgrade pip
+#pip install torch==1.5.0+cu92 torchvision==0.6.0+cu92 -f https://download.pytorch.org/whl/torch_stable.html
+#cd /home/u11/mithunpaul/mean-teacher/student_teacher
+#pip install -r requirements.txt
+
+
 
 
 #####my code part
-export PYTHONPATH="/home/u11/mithunpaul/sentence-transformers/"
+export PYTHONPATH="/home/u11/mithunpaul/mean-teacher/"
 
 #cd /home/u11/mithunpaul/sentence-transformers/examples
 

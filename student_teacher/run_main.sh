94b7b4bd1d9d3d4db43af6eba3271bfcccdff1cc
mithun <mithunpaul08@gmail.com>
2020-05-26 01:49:53 -0700

    randomseed=337+batchsize=32+delex

--- student_teacher/run_main.sh
+++ student_teacher/run_main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-python main.py --add_student True  --use_ema False --load_model_from_disk_and_test False  --lex_train_full_path fever/train/fever_train_lex.jsonl --random_seed 45674  --create_new_comet_graph True --save_dir '/xdisk/msurdeanu/mithunpaul/model_storage/'
+python main.py --add_student True  --use_ema False --load_model_from_disk_and_test False  --lex_train_full_path fever/train/fever_train_lex.jsonl --random_seed 337  --create_new_comet_graph True --save_dir '/xdisk/msurdeanu/mithunpaul/model_storage/'
